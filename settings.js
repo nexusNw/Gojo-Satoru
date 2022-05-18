e9bc94a09f904acd82de2162547d1825069fde6e
TOXIC ALIEN <64305844+Alien-alfa@users.noreply.github.com>
2022-05-18 11:06:38 +0530

    API update

--- settings.js
+++ settings.js
@@ -15,7 +15,7 @@ global.APIs = {
 
 //Api Key\\
 global.APIKeys = {
-	'https://zenzapis.xyz': 'd6295ca53e', // if api key expire, buy it and put it here?\\
+	'https://zenzapis.xyz': 'sanzychan01', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
 }
 
 //customize settings\\
