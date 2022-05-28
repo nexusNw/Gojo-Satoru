572e6bd1df6dc5632ababd72954212db9372d65b
Kingsley <100943735+Ife250@users.noreply.github.com>
2022-05-29 00:58:39 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,73 +1,146 @@
 //═══════════════════════════════════════════════════════//
+
 //
+
 //                              𝙶𝙾𝙹𝙾-𝚂𝙰𝚃𝙾𝚁𝚄 𝓫𝔂 𝓷𝓮𝔁𝓾𝓼𝓝𝔀
+
 //𝙰𝙳𝙾𝙿𝚃𝙴𝙳 𝙵𝚁𝙾𝙼  𝚂𝙲𝚁𝙸𝙿𝚃 𝙾𝙵 𝙲𝙷𝙴𝙴𝙼𝚂𝙱𝙾𝚃 𝚅2 𝙱𝚈 𝙳𝙶𝚇𝚎𝚘𝚗 
+
 //
+
 //════════════════════════════//
 
+
+
 const fs = require('fs')
+
 const chalk = require('chalk')
 
+
+
 //Api Website\\
+
 global.APIs = {
+
 	zenz: 'https://zenzapis.xyz',
 }
 
+
+
 //Api Key\\
+
 global.APIKeys = {
+
 	'https://zenzapis.xyz': 'sanzychan01', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
+
 }
 
+
+
 //customize settings\\
+
 global.owner = ['265889976968']
-global.premium = ['265889976968']
+
+global.premium = ['265889976968,265883776711,265994894966,263715183177']
+
 global.ownernomer = '265889976968'
+
 global.ownername = '🐦kingsley'
+
 global.botname = 'kingsley-bot'
+
 global.footer = '©kingsleyInc.'
+
 global.ig = 'https://github.com/ife250'
+
 global.region = 'malawi, South africa, blantyre'
+
 global.sc = 'https://github.com/ife250/Gojo-Satoru'
+
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
+
 global.packname = 'kingsley-bot'
+
 global.author = '🐦kingsley'
+
 global.sessionName = 'session'
-global.prefa = ['-','.','🐦','🐤','🗿']//prefix
+
+global.prefa = ['👩‍💻','-','.','🐦','🐤','🗿']
+
 global.sp = '⭔'
+
 global.mess = {
+
     success: 'kingsley 👩‍💻 Done ✓',
+
     admin: 'This Feature Is Only For Admin!',
+
     botAdmin: 'Bot Must Be Admin First!',
+
     owner: 'This Feature Is Only For ꪶ࿋྄ིᤢꫂKíñgsléyꪶ࿋྄ིᤢꫂ https://wa.me/265889976968',
+
     group: 'Feature Used Only For Groups lmao!',
+
     private: 'Features Used Only For Private Chat!',
+
     bot: 'This Feature Is Only For Bot',
+
     wait: 'Wait A minute kingsley on the way...',
+
     error: 'Error! Maybe Api Key Is Expired🤔!',
+
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+
 }
+
     global.limitawal = {
+
     premium: "Infinity",
+
     free: 12,
+
     monayawal: 1000
+
 }
+
    global.rpg = {
+
    darahawal: 100,
+
    besiawal: 15,
+
    goldawal: 10,
+
    emeraldawal: 5,
+
    umpanawal: 5,
+
    potionawal: 1
+
 }
+
 global.thumb = fs.readFileSync('./GojoMedia/AnimeX.jpg')
+
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
 global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
 global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
 let file = require.resolve(__filename)
+
 fs.watchFile(file, () => {
+
 	fs.unwatchFile(file)
+
 	console.log(chalk.redBright(`Update'${__filename}'`))
+
 	delete require.cache[file]
+
 	require(file)
+
 })
+
+
