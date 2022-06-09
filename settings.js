3a2dab67d089a31a2d924a1ba6384b6d8b5e694a
lgazary <106930262+lgazary@users.noreply.github.com>
2022-06-09 11:47:41 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,16 +19,16 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.owner = ['01098906252']
+global.premium = ['01098906252']
+global.ownernomer = '01098906252'
+global.ownername = 'elgazar alwazer💕'
+global.botname = 'elgazar alwazer💕'
+global.footer = 'elgazar alwazer💕.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
+global.myweb = 'https://youtube.com/channel/UCxVaIay8BccgBtsofagA6_g'
 global.packname = 'Gojo-Satoru'
 global.author = '🐦Star Boy Nexus'
 global.sessionName = 'session'
