baebd7d889163da72711ac56d970c7dff360790a
Anu68 <107714277+Anu68@users.noreply.github.com>
2022-06-18 06:33:27 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['94770267442']
+global.premium = ['94770267442']
+global.ownernomer = '94770267442'
 global.ownername = '🐦Star Boy Nexus'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
