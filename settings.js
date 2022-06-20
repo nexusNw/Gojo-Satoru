b503a279dac3bd0be4ec97a5ec6f9c8c9158f006
tharumin <94857655+tharumin@users.noreply.github.com>
2022-06-20 23:46:50 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,16 +19,16 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94704421161,94779062397']
-global.premium = ['94704421161']
-global.ownernomer = '94704421161,94779062397'
+global.owner = ['94779062397']
+global.premium = ['94779062397']
+global.ownernomer = '94779062397'
 global.ownername = '🆂🅰🅳🅴🅴🆂🅷🅰'
 global.botname = 'ᴀʟᴘʜᴀ ǫᴜᴇᴇɴ'
-global.footer = '©NexusNwInc.'
+global.footer = '©Alpha.'
 global.ig = 'https://github.com/sadiyamin'
 global.region = 'Sri Lankan, South , Walasmulla'
-global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
+global.sc = 'https://github.com/sadiyamin'
+global.myweb = 'Yt Chanel එකක්.නෑ...☹️'
 global.packname = 'Sadeesha😞'
 global.author = 'කෙල්ලෙක් ඕනි..😁🙈'
 global.sessionName = 'session'
@@ -36,13 +36,13 @@ global.prefa = ['','!','.','📚','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
     success: 'Done ✓(හරි(ᴍɪɴ ᴛᴇᴄʜ ᴅᴇᴠᴇʟᴏᴘᴇʀ sᴀᴅᴇᴇsʜᴀ)',
-    admin: 'This Feature Is Only For Admin! _(Adminලට විතරයි හුත්තො 😂)_',
+    admin: '_Adminලට විතරයි හුත්තො 😂_',
     botAdmin: 'Bot Must Be Admin First!',
-    owner: 'This Feature Is Only For Owner!(සදීෂට විතරයි...😏)',
-    group: 'Feature Used Only For Groups!(Group වලට විතරයි...🙄)',
+    owner: '_සදීෂට විතරයි...😏_',
+    group: '_Group වලට විතරයි...🙄_',
     private: 'Features Used Only For Private Chat!',
-    bot: 'This Feature Is Only For Bot(සදීෂට විතරයි...🙈)',
-    wait: 'Wait A minute... _(පොඩ්ඩක් ඉන්න මැණික...🥺)_',
+    bot: '_සදීෂට විතරයි...🙈_',
+    wait: '_(පොඩ්ඩක් හිටපං...😏)_',
     error: 'Error! Maybe Api Key Is Expired🤔! _(අවුලක් සගෝ...ආයෙ ඉල්ලන්න එපා...😏)_',
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
 }
