36ede3e12a4863ab1035f538ecd677c3adee7e24
MrOutis <108561034+MrOutis@users.noreply.github.com>
2022-07-01 22:53:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -140,12 +140,15 @@ async function startGojoMdNx() {
         let nama = await GojoMdNx.getName(num)
 memb = metadata.participants.length
 
+Kon = await getBuffer(`https://hardianto.xyz/api/welcome3?profile=${encodeURIComponent(ppuser)}&name=${encodeURIComponent(nama)}&bg=https://telegra.ph/file/8bbe8a7de5c351dfcb077.jpg&namegb=${encodeURIComponent(metadata.subject)}&member=${encodeURIComponent(memb)}`)
+
+Tol = await getBuffer(`https://hardianto.xyz/api/goodbye3?profile=${encodeURIComponent(ppuser)}&name=${encodeURIComponent(nama)}&bg=https://telegra.ph/file/8bbe8a7de5c351dfcb077.jpg&namegb=${encodeURIComponent(metadata.subject)}&member=${encodeURIComponent(memb)}`)
                 if (anu.action == 'add') {
                     GojoMdNx.sendMessage(anu.id, { image: Kon, contextInfo: { mentionedJid: [num] }, caption: `
-⭐✑ 𝗛𝗼𝗹𝗮 👋🏻 @${num.split("@")[0]},
-⭐✑ 𝗕𝗶𝗲𝗻𝘃𝗲𝗻𝗶𝗱𝗼 𝗮𝗹 𝗴𝗿𝘂𝗽𝗼 ${metadata.subject}
+✑ 𝗛𝗼𝗹𝗮 👋🏻 @${num.split("@")[0]},
+✑ 𝗕𝗶𝗲𝗻𝘃𝗲𝗻𝗶𝗱𝗼 𝗮𝗹 𝗴𝗿𝘂𝗽𝗼 ${metadata.subject}
 
-✑ No te olvides de leerlas reglas del grupo hee para asegurar tu estadia!!`} )
+✑ No te olvides de leer las reglas del grupo!!!`} )
                 } else if (anu.action == 'remove') {
                     GojoMdNx.sendMessage(anu.id, { image: Tol, contextInfo: { mentionedJid: [num] }, caption: `✑ @${num.split("@")[0]} Adiós ${metadata.subject}
 
