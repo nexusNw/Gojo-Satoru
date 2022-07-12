3a2dbcbf71336889ef64aac8a4510d55ad4e7c4b
Hashastatus <109164074+Hashastatus@users.noreply.github.com>
2022-07-12 21:35:38 +0530

    Upload hasha

--- settings.js
+++ settings.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['bot hasha']
+global.premium = ['bot hasha']
+global.ownernomer = 'bot hasha'
 global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.botname = 'Bot Hasha'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
