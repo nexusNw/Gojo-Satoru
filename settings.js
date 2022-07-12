6e437cd9a0f164fe7e045ca6f833d686600459e3
Hashastatus <109164074+Hashastatus@users.noreply.github.com>
2022-07-12 21:47:09 +0530

    hasha up
    Hashanaya

--- settings.js
+++ settings.js
@@ -19,17 +19,17 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['Hashan']
+global.premium = ['Hashan']
+global.ownernomer = 'Hashan'
+global.ownername = '💦Bot Hasha🇱🇰'
+global.botname = 'Bot Hasha'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
+global.packname = 'Bot-Hasha'
 global.author = '🐦Star Boy Nexus'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
